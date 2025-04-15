1c28c6f20698c4d61ab0ddac00f87839a6c79bac
weiuou <weiuou@qq.com>
2025-04-15 16:42:58 +0800

    创建前后端项目

---
+++ ot_doc/src/main/java/xyz/weiuou/ot_doc/OtDocApplication.java
@@ -0,0 +1,13 @@
+package xyz.weiuou.ot_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtDocApplication.class, args);
+	}
+
+}
---
+++ ot_doc/src/test/java/xyz/weiuou/ot_doc/OtDocApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.weiuou.ot_doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
